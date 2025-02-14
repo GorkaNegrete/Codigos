40149abc2ca1b9784a096a5c5bcd4a31ffa6640c
GorkaNegrete <negretetelleriagorka@gmail.com>
2025-02-13 20:07:58 -0600

    Update main.py
    Se agrego el calculo del volumen de la esfera

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ def calcular_perimetro_circulo(radio):
 radio = 7
 perimetro = calcular_perimetro_circulo(radio)
 print(f"El perímetro del círculo con radio {radio} es {perimetro}")
+def calcular_volumen_esfera(radio):
+    return (4/3) * math.pi * (radio ** 3)
+
+# Ejemplo de uso
+volumen = calcular_volumen_esfera(radio)
+print(f"El volumen de la esfera con radio {radio} es {volumen}")
