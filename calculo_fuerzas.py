fbb325dbb15ced3148a805c221fd155c15ca7ea2
GorkaNegrete <negretetelleriagorka@gmail.com>
2025-03-13 01:06:38 -0600

    Limpieza

--- calculo_fuerzas.py
+++ calculo_fuerzas.py
@@ -2,12 +2,9 @@ import numpy as np
 import scipy as sp
 import sympy as sym
 import math
-
 atotal_mm = np.array([4.6437, 2.4073, 4.7184])
 atotal_m = atotal_mm / 1000
-
 mass = .368
-
 gravity = 9.81
 z_vector = np.array([0, 0, 1])
 F_gripper_v = (mass * atotal_m)+(mass*gravity*z_vector)
